0d6ddca3fe7300c8454dd6b675fdd0507e5fb7b4
TheSebasx11 <sebabalcar18@gmail.com>
2021-03-21 16:40:15 -0500

    Update regla_falsa_acti.m

--- Fabián/regla_falsa_acti.m
+++ Fabián/regla_falsa_acti.m
@@ -1,22 +1,33 @@
 clc 
 clear all
 
+%Pedimos la función
 w = input("Ingrese su funcion: ", "S");
+%Convertimos en función
 fuc = inline(w);
+%pedimos los datos //
 disp("Ingrese el intervalo: ");
 a = input("Ingrese el primer valor del intervalo: ");
 b = input("Ingrese el segundo valor del intervalo: ");
 e_usuario = input("ingrese el error inicial: ");
+%//
+%Evaluamos los puntos en la función
 fa = fuc(a);
 fb = fuc(b);
+%Variable iteradora y Xr primero
 ite = 1;
 xr=0;
+
+%Evaluamos esa mondá
 if (fa*fb<0)
-  fprintf("i   Xn-1      Xn        Xn+1  f(Xn)     |Ea|\n");  
+  fprintf("i   Xn-1      Xn          f(Xn)     |Ea|\n");  
   while(true)
+    %Hallamos la xr y evaluamos xr
     xr = b - ((fb)*(a-b)/(fa - fb));
     fxr = fuc(xr);
-    if (ite < 2)
+  %verificamos que iteración es.
+  if (ite < 2)
+  %hacemos el cambio de coordenadas
       if (fa*fxr<0)
         b = xr;
         xp = b;
@@ -26,17 +37,20 @@ if (fa*fb<0)
           xp = a;
         end
       end
-      fprintf("%d   %0.5f   %0.5f   Xn+1  %0.5f   ---- \n",ite,xp,xr,fxr);
+      %añadimos a la tabla de datos y sumamos 1na iteración
+      fprintf("%d   %0.5f   %0.5f     %0.5f   ---- \n",ite,xp,xr,fxr);
       ite = ite + 1;
     else
+      %Empezamos a calcular los errores
       Ea = abs(((xr-xp)/xr))*100;
+      %Verificamos si cumplimos con la condicion del error del usuario
       if(Ea<=e_usuario)
         break;
       else
+        %Seguimos partiendo el intervalo
         if (fa*fxr<0)
           b = xr;
-          xp = b;
-          
+          xp = b;   
         else 
           if (fa*fxr>0)
             a = xr;
@@ -44,10 +58,11 @@ if (fa*fb<0)
           end
         end
       end
-      fprintf("%d   %0.5f   %0.5f   Xn+1  %0.5f   %0.5f \n",ite,xp,xr,fxr,Ea);
+      %Añadimos a la tabla y sumamos una iteración
+      fprintf("%d   %0.5f   %0.5f     %0.5f   %0.5f \n",ite,xp,xr,fxr,Ea);
       ite = ite + 1;
     end
-    pause(1);
+    %pause(1);
   end
   fprintf("Finalmente tenemos la raiz: Xr = %0.7f",xr);
 else
